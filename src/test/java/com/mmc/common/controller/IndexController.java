d344d150ce664acbd46a3eba41966765bf587739
SummerMMC <liusongning@hotmail.com>
2020-12-24 14:43:46 +0800

    change

--- src/test/java/com/mmc/common/controller/IndexController.java
+++ src/test/java/com/mmc/common/controller/IndexController.java
@@ -1,28 +1,19 @@
 package com.mmc.common.controller;
 
-import com.mmc.common.Application;
-import com.mmc.common.service.UserService;
+import com.mmc.common.base.JSONUtil;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+import java.util.Map;
+
 @ResponseBody
 public class IndexController {
-
-    @Autowired
-    private UserService userService;
-
     @Test
     public void test() throws Exception {
-//        try {
-        userService.test();
-//        } catch (Exception e) {
-//            e.getMessage();
-//        }
+        String str = "{name:'mmc'}";
+        Map<String, Object> m = JSONUtil.getJSONField(str);
+        for(Object key : m.keySet()){   //只能遍历key
+            System.out.print(m.get("name"));
+        }
     }
 }
